6956ebe7f944eb530e09a0d2394bc7b1c1b0041f
Ankush thakur <ankushthakur302003@gmail.com>
2026-09-22 10:51:42 +0530

    Update ankush project.py

--- ankush project.py
+++ ankush project.py
@@ -6,7 +6,6 @@ def generate_password(length):
     letters = string.ascii_letters   
     digits = string.digits
     symbols = string.punctuation    
-
     
     all_characters = letters + digits + symbols
 
